bf2f4049833dab11c4dc81f11b92caaa0992b8ca
JWC <103419525+DataRaptor@users.noreply.github.com>
2022-07-16 02:30:26 -0400

    Switched to mongodb. Defined orm and cleaned up handlers.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,25 +1,13 @@
 require("dotenv").config()
 import express from "express"
 import axios from "axios"
-import { Request } from "node-fetch";
 import bodyParser = require('body-parser')
 import { sign } from 'tweetnacl';
 const cors = require('cors')
 import bs58 from "bs58"
-// import { PublicKey } from "@s"
-
 import url from "url"
-
-const app = express();
-
-app.use(
-bodyParser.urlencoded({
-    extended: true,
-})
-)
-app.use(express.json())
-app.use(cors())
-
+const mongoose = require('mongoose');
+import { User } from "./models"
 
 const DISCORD_API: string = "https://discord.com/api/v8"
 
@@ -28,70 +16,87 @@ const DISCORD_CLIENT_ID: string = process.env.DISCORD_CLIENT_ID || ""
 const DISCORD_SECRET: string = process.env.DISCORD_SECRET || ""
 const DISCORD_REDIRECT_URI: string = process.env.DISCORD_REDIRECT_URI || ""
 const DRIFT_MESSAGE: string = process.env.DRIFT_MESSAGE || "Default Drift Message"
+const MONGO_CONN_STRING: string = process.env.MONGO_CONN_STRING || ""
+const CLIENT_URL: string = process.env.CLIENT_URL || "http://localhost:3000"
 
-console.log({
-    client_id: DISCORD_CLIENT_ID,
-    client_secret: DISCORD_SECRET,
-    grant_type: 'authorization_code',
-    redirect_uri: DISCORD_REDIRECT_URI
-})
+const main = async() => {
+    await mongoose.connect(MONGO_CONN_STRING);
+    const app = express();
+    app.use(
+        bodyParser.urlencoded({
+        extended: true,
+    })
+    )
+    app.use(express.json())
+    app.use(cors())
 
-
-app.get('/v1/discord_redirect', async(req: express.Request, res: express.Response) => {
-    const { code } = req.query
-    const tokenResponse = await axios.post(`${DISCORD_API}/oauth2/token`, new url.URLSearchParams({
-        client_id: DISCORD_CLIENT_ID,
-        client_secret: DISCORD_SECRET,
-        grant_type: 'authorization_code',
-        code: code.toString(),
-        redirect_uri: DISCORD_REDIRECT_URI
-    }).toString(),
-    {
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
+    app.get('/v1/discord_redirect', async(req: express.Request, res: express.Response) => {
+        try {
+            const { code } = req.query
+            const urlSearchParams = new url.URLSearchParams({
+                client_id: DISCORD_CLIENT_ID,
+                client_secret: DISCORD_SECRET,
+                grant_type: 'authorization_code',
+                code: code.toString(),
+                redirect_uri: DISCORD_REDIRECT_URI
+            }).toString()
+            const tokenResponse = await axios.post(`${DISCORD_API}/oauth2/token`, urlSearchParams,
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            })
+            const { access_token } = tokenResponse.data
+            res.redirect(`${CLIENT_URL}?access_token=${access_token}`)
+        } catch(error) {
+            // If something goes wrong, send them back to the client
+            res.redirect(CLIENT_URL)
         }
     })
-    // Now let's grab the user's access token and pass it back to the client via url parameter
-    // This is a secure method because SSL will protect against query parameters in transit. 
-    // However it will appear in our server logs... This is bad... We will revoke the token 
-    // on the following request which should mean that this is safe provided that the user 
-    // the bottom request is executed when the user returns to the page. 
-    // This kind of jankness seems is only really required because we have signature verification...
-    // Otherwise we'd be alright with just using our server to consume the data to the db here...
-    const { access_token } = tokenResponse.data
-    res.redirect(`http://localhost:3000?access_token=${access_token}`)
-})
 
 
-app.post('/v1/create_discord_user', async(req: express.Request, res: express.Response) => {
-    // first let's get the auth token
-    console.log("---> ACCESS_TOKEN", req.body)
-    const { accessToken, signature, publicKey } = req.body
-
-    console.log("---> ACCESS_TOKEN", accessToken)
-
-    // This is where we want to post this stuff to the Database...
-    const message = new TextEncoder().encode(DRIFT_MESSAGE);
-    if (sign.detached.verify(message, bs58.decode(signature), bs58.decode(publicKey))){
-        const userResponse = await axios.get(`${DISCORD_API}/users/@me`, {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
+    app.post('/v1/create_discord_user', async(req: express.Request, res: express.Response) => {
+        try{
+            const { accessToken, signature, publicKey } = req.body
+            const message = new TextEncoder().encode(DRIFT_MESSAGE);
+            if (sign.detached.verify(message, bs58.decode(signature), bs58.decode(publicKey))){
+                const userResponse = await axios.get(`${DISCORD_API}/users/@me`, {
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                })
+                const discordUserData = userResponse.data
+                const query = await User.find({
+                    public_key: publicKey,
+                    ...discordUserData
+                })
+                if (query.length == 0){
+                    // We opt to save the discord data everytime the user comes back to us with a new public_key
+                    // So that we can later associate public_keys by discord_ids. Hence this query.
+                    const user: typeof User = new User({
+                        public_key: publicKey,
+                        signature: signature,
+                        message: message,
+                        ...discordUserData
+                    })
+                    await user.save().then(() => console.log("saved!!!"))
+                    res.status(200).json({"message": "You've successfully linked your discord to Drift! Welcome"})
+                } else {
+                    res.status(200).json({"message": "Your discord is already registed with Drift!"})
+                }
+            } else {
+                res.status(500).send({
+                    "message": "Invalid Signature for Public Key" 
+                })
             }
-        })
-        console.log("SSUCCEEEESSS!!", userResponse.data)
-    } else {
-        console.log("FAILUREEEE")
-    }
-    
-    res.send({'ok': true})
-})
+        } catch(error) {
+            res.status(500).json({
+                "message": "Something went wrong. Please try again."
+            })
+        }
+    })
 
-// app.get('/v1/discord_user', async(req: express.Request, res: express.Response) => {
+    app.listen(PORT, async() => console.log("API started..."))
+}
 
-// })
-
-
-
-app.listen(PORT, () => {
-    console.log("API started...")
-})
+main()
