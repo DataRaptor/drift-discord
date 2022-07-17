823458383d0f6f716d24fc065d6bfc27b2b7f0f2
JWC <103419525+DataRaptor@users.noreply.github.com>
2022-07-17 05:25:07 -0400

    CHORE: Deploy Failyed. Debugging.

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,4 +1,5 @@
 require('dotenv').config()
+import { logger } from "../services"
 
 export const PORT = process.env.PORT || 8080
 export const MONGO_CONN_STRING: string = process.env.MONGO_CONN_STRING || ''
@@ -16,3 +17,17 @@ export const DRIFT_MESSAGE: string =
 export const CLIENT_URL: string =
       process.env.CLIENT_URL || 'http://localhost:3000'
 export const AES_HTTP_TRANSPORT_SECRET: string = process.env.AES_HTTP_TRANSPORT_SECRET || "11122233344455566677788822244455555555555555555231231321313aaaff" 
+
+logger.error("LOGGING SOME NON SECRET ENV VARIABLES TO DEBUG DEPLOY.")
+
+logger.error({
+      "$$$ PORT": PORT
+})
+
+logger.error({
+      "$$$ DISCORD_API": DISCORD_API
+})
+
+logger.error({
+      "$$$ DISCORD_REDIRECT_URI": DISCORD_REDIRECT_URI
+})
